13489fa82d94139b339ed016476f94e629d96b76
mariah <mariah@gardziola.com>
2019-05-29 21:26:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def count_with_sleep(sec)
+  sleep(sec)
 end
+  
+def countdown(number)
+  
+   number = 10
+
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    count_with_sleep(10)
+  end
+  
+  "HAPPY NEW YEAR!"
+end
+
